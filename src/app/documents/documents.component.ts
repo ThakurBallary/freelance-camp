6d5d2a0b6a7da8700d4ecfacd60c4d76ecbd35cc
Thakur <thakurballary@gmail.com>
2017-10-16 22:57:09 +0530

    Before adding rails api

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
 	selector: 'documents',
-	templateUrl: 'documents.component.html'
+	templateUrl: 'documents.component.html',
+	styleUrls: ['documents.component.css']
 })
 export class DocumentsComponent {
-	
+	pageTitle: string = "Document Dashboard";
+	documents: Document[] = [
+		{
+			title: "My First Doc",
+			description: "asdf asdfjksjflksj",
+			file_url: "http://google.com",
+			updated_at: "15/10/2017",
+			image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
+		},
+		{
+			title: "My Second Doc",
+			description: "asdf asdfjksjflksj",
+			file_url: "http://google.com",
+			updated_at: "15/10/2017",
+			image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
+		},
+		{
+			title: "My Third Doc",
+			description: "asdf asdfjksjflksj",
+			file_url: "http://google.com",
+			updated_at: "15/10/2017",
+			image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
+		},
+	]
 }
---
+++ src/app/proposal/proposal-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Proposal } from './proposal';
+
+@Component({
+	selector: 'proposal-list',
+	templateUrl: 'proposal-list.component.html',
+	styleUrls: ['proposal-list.component.css']
+})
+export class ProposalListComponent {
+	proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+	proposalTwo: Proposal = new Proposal(90, 'XYZ Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+	proposalThree: Proposal = new Proposal(300, 'MN Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+
+	proposals: Proposal[] = [
+		this.proposalOne, 
+		this.proposalTwo, 
+		this.proposalThree
+	]
+}
