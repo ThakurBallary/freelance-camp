9b66fce579fee21f548ba4ed5db5d3b969f19b03
Thakur <thakurballary@gmail.com>
2017-10-16 14:37:20 +0530

    + Links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+	// moduleId: module.id,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App';
+  // title = 'Fe - Dashboard';
 }
