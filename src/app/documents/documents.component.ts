db3dc8d388c52a5c128f39588ece22a3148bdb5e
Thakur <thakurballary@gmail.com>
2017-10-18 22:03:17 +0530

    Completed

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,34 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
 	selector: 'documents',
 	templateUrl: 'documents.component.html',
-	styleUrls: ['documents.component.css']
+	styleUrls: ['documents.component.css'],
+	providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
 	pageTitle: string = "Document Dashboard";
-	documents: Document[] = [
-		{
-			title: "My First Doc",
-			description: "asdf asdfjksjflksj",
-			file_url: "http://google.com",
-			updated_at: "15/10/2017",
-			image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
-		},
-		{
-			title: "My Second Doc",
-			description: "asdf asdfjksjflksj",
-			file_url: "http://google.com",
-			updated_at: "15/10/2017",
-			image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
-		},
-		{
-			title: "My Third Doc",
-			description: "asdf asdfjksjflksj",
-			file_url: "http://google.com",
-			updated_at: "15/10/2017",
-			image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
-		},
-	]
+	documents: Document[];
+	errorMessage: string;
+	mode = 'Observable';
+
+	constructor(
+		private documentService: DocumentService
+	){}
+
+	ngOnInit() {
+		let timer = Observable.timer(0, 60000);
+		timer.subscribe(() => this.getDocuments());
+	}
+
+	getDocuments() {
+		this.documentService.getDocuments()
+			.subscribe(
+				documents => this.documents = documents,
+				error => this.errorMessage = <any>error
+			);
+	}
 }
--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
 @Component({
 	selector: 'proposal-list',
 	templateUrl: 'proposal-list.component.html',
-	styleUrls: ['proposal-list.component.css']
+	styleUrls: ['proposal-list.component.css'],
+	providers: [ ProposalService ]
 })
-export class ProposalListComponent {
-	proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
-	proposalTwo: Proposal = new Proposal(90, 'XYZ Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
-	proposalThree: Proposal = new Proposal(300, 'MN Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+export class ProposalListComponent implements OnInit {
+	proposals: Proposal[];
+	errorMessage: string;
+	mode = 'Observable';
 
-	proposals: Proposal[] = [
-		this.proposalOne, 
-		this.proposalTwo, 
-		this.proposalThree
-	]
+	constructor(
+		private proposalService: ProposalService,
+		private router: Router
+	){}
+
+	ngOnInit() {
+		let timer = Observable.timer(0, 60000);
+		timer.subscribe(() => this.getProposals());
+	}
+
+	getProposals() {
+		this.proposalService.getProposals()
+			.subscribe(
+				proposals => this.proposals = proposals,
+				error => this.errorMessage = <any>error
+			);
+	}
+
+	goToShow(proposal: Proposal): void {
+		let link = ['/proposal', proposal.id];
+		this.router.navigate(link);
+	}
 }
